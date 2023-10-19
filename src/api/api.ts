4dae3aef7b2247cb4e1026108d73893953f70c2f
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2023-10-19 15:46:18 +0300

    fix: change base url

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,8 @@ const token = getToken();
 
 export const instance = axios.create({
     // baseURL: 'https://m-app-back-production.up.railway.app/',
-    baseURL: 'https://m-app-back.herokuapp.com/',
+    // baseURL: 'https://m-app-back.herokuapp.com/',
+    baseURL: 'https://management-app-back-6474cd70183b.herokuapp.com/',
     withCredentials: false,
     headers: {
         Authorization: `Bearer ${token}`
